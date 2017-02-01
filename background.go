459069aff79b32b9a6f2139a26d04a7464f98f78
Yash Patel <ypatel@princeton.edu>
2017-02-01 12:48:45 -0500

    Added rendering of the ground

--- background.go
+++ background.go
@@ -7,6 +7,28 @@ import (
 	"github.com/SimonWaldherr/golibs/as"
 )
 
+// global variables used for rendering different "objects" in scene
+var SKY    = 0
+var GROUND = 1
+var CLOUD  = 2
+var PLAYER = 3
+var ENEMY  = 4
+
+// variables for adjusting window size
+var WIDTH  = 100
+var HEIGHT = 30
+
+// global variables used for background "sections" -- marks height btween which
+// certain background characteristics occur
+// ---------------------------
+// -----       SKY       -----
+// -----      CLOUD      -----
+// ---------------------------
+// -----     GROUND      -----
+// ---------------------------
+var CLOUD_LEVEL  = []int{20,30}
+var GROUND_LEVEL = []int{0,10}
+
 // object containing the background of the game -- also contains
 // variables about what is in each position on the board
 type Background struct {
@@ -28,19 +50,22 @@ type Background struct {
 }
 
 func initBackground() *Background {
-	width := 100
-	height:= 30
 	board := [][]int{}
 
-	for i := 0; i < width; i++ {
-		row := make([]int, height)
+	for i := 0; i < HEIGHT; i++ {
+		row := make([]int, WIDTH)
+		if i >= GROUND_LEVEL[0] && i < GROUND_LEVEL[1] {
+			for j := 0; j < WIDTH; j++ {
+				row[j] = GROUND
+			}
+		} 
 		board = append(board, row)
 	}
 
 	return &Background{
-		width:  width,
-		height: height,
-		board: board,
+		width:  WIDTH,
+		height: HEIGHT,
+		board:  board,
 
 		speed: 1,
 		score: 0,
@@ -48,9 +73,25 @@ func initBackground() *Background {
 	}
 }
 
-func insertOnBoard(background *Background, positions [][]int, identifier int) *Background {
-	PAIR_SIZE := 2
+func insertOnBoard(background *Background, positions [][]int, id string) *Background {
+	var identifier int
+	switch id {
+	case "sky":
+		identifier = SKY
+	case "tree":
+		identifier = GROUND
+	case "cloud":
+		identifier = CLOUD
+	case "player":
+		identifier = PLAYER
+	case "enemy":
+		identifier = ENEMY
+	default:
+		fmt.Println("Error: Not a valid object type!")
+		return background
+	}
 
+	PAIR_SIZE := 2
 	for i := 0; i < len(positions); i++ {
 		curPosition := positions[i]
 		if len(curPosition) != PAIR_SIZE {
@@ -63,25 +104,37 @@ func insertOnBoard(background *Background, positions [][]int, identifier int) *B
 	return background
 } 
 
-func render(background *Background) string {
-	SKY    := 0
-	TREE   := 1
-	CLOUD  := 2
-	PLAYER := 3
-	ENEMY  := 4
-
-	var buffer bytes.Buffer
+func moveBackground(background *Background) {
 	for y := 0; y < background.height; y++ {
+		// old part of background that's shifting/moving at set speed
+		for x := 0; x < background.width - background.speed; x++ {
+			background.board[x][y] = background.board[x + background.speed][y]
+		}
+
+		// new background being generated to replace the old background
+		for x := background.width - background.speed; x < background.width; x++ {
+			background.board[x][y] = SKY
+		}
+	}
+}
+
+func insertClouds(background *Background) {
+	return
+}
+
+func render(background *Background) {
+	var buffer bytes.Buffer
+	for y := background.height - 1; y >= 0; y-- {
 		for x := 0; x < background.width; x++ {
-			if background.board[x][y] == SKY {
+			if background.board[y][x] == SKY {
 				buffer.Write(as.Bytes(ansi.Color("█", ansi.Blue)))
-			} else if background.board[x][y] == TREE {
-				buffer.Write(as.Bytes(ansi.Color("█", ansi.Red)))
-			} else if background.board[x][y] == CLOUD {
+			} else if background.board[y][x] == GROUND {
+				buffer.Write(as.Bytes(ansi.Color("█", ansi.Green)))
+			} else if background.board[y][x] == CLOUD {
 				buffer.Write(as.Bytes(ansi.Color("█", ansi.White)))
-			} else if background.board[x][y] == PLAYER {
+			} else if background.board[y][x] == PLAYER {
 				buffer.Write(as.Bytes(ansi.Color("█", ansi.Blue)))
-			} else if background.board[x][y] == ENEMY {
+			} else if background.board[y][x] == ENEMY {
 				buffer.Write(as.Bytes(ansi.Color("█", ansi.Red)))
 			} 
 		}
